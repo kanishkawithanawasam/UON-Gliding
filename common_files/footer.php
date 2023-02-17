4bcc0c364391d4f50b00c302108775081ac3b61a
kanishkawithanawasam <117766167+kanishkawithanawasam@users.noreply.github.com>
2023-02-17 17:30:02 +0000

    Social Links were added

--- common_files/footer.php
+++ common_files/footer.php
@@ -1,13 +1,13 @@
 <footer>
         <div class="social">
             <div>
-                <a href="http://"><i class="bi bi-facebook"></i></a>
+                <a href="https://www.facebook.com/groups/NUGlidingClub/?ref=bookmarks"><i class="bi bi-facebook"></i></a>
             </div>
             <div>
-                <a href="http://"><i class="bi bi-instagram"></i></a>
+                <a href="https://www.instagram.com/uon_gliding/"><i class="bi bi-instagram"></i></a>
             </div>
             <div>
-                <a href="http://"><i class="bi bi-twitter"></i></a>
+                <a href="https://twitter.com/uongliding?lang=en"><i class="bi bi-twitter"></i></a>
             </div>
         </div>
     </footer>
