868b8aadf0e189b01faa89f5c02e967258a890b4
Kanishka Withanawasam <kanishkawithanawasam@gmail.com>
2023-06-15 21:17:33 +0100

    Update on navigation and index

--- common_files/headfiles.php
+++ common_files/headfiles.php
@@ -9,3 +9,4 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
